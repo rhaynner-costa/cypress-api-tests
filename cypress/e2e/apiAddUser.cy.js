e4d91171360fbe16baefa46b576b46f568c57b67
rca <rhaynner@gmail.com>
2024-06-21 09:20:49 -0300

    criados cenarios 2 e 3

--- cypress/e2e/apiAddUser.cy.js
+++ cypress/e2e/apiAddUser.cy.js
@@ -19,7 +19,41 @@ describe("API tests that add new users", () => {
       })
 
     })
- 
+    
+    //adicionar um novo usuario, enviando o nome no body em branco
+    it('Test 2 - Add a new user by sending an empty name in the request body', ()=>{
+      cy.request({
+        method:'POST',
+        url: 'https://reqres.in/api/users',
+        body: {
+            "name": "",
+            "job": "Manager"
+        }
+      }).then((response)=>{
+        expect(response.status).to.equal(201)
+        expect(response.body.name).to.equal('')
+        expect(response.body.job).to.equal('Manager')
+        expect(response.body.id).not.be.null
+
+      })
+    })
+
+    //adicionar um novo usuario, enviando o job no body em branco
+    it('Test 3 - Add a new user by sending an empty job in the request body', ()=>{
+      cy.request({
+        method:'POST',
+        url: 'https://reqres.in/api/users',
+        body: {
+            "name": "Rhaynner",
+            "job": ""
+        }
+      }).then((response)=>{
+        expect(response.body.name).to.equal('Rhaynner')
+        expect(response.body.job).to.equal('')
+        expect(response.body.id).not.be.null
+
+      })
+    })
 
 })
 
@@ -28,10 +62,10 @@ describe("API tests that add new users", () => {
 
 
  
-    //adicionar um novo usuario, enviando o nome no body em branco
-    //adicionar um novo usuario, enviando o job no body em branco
+   
+    
     //test do jasonSchema
-    //vaiidar o status code
+    //validar o status code
     //validar a data de cadastrado, deve ser iqual a data de hoje
     //validar se retornou o id 
     //fazer o assert no nome do usuario cadastrado
