691f3e5a5cc88508dadaf0822d1d33b696954f9e
rca <rhaynner@gmail.com>
2024-06-21 08:30:27 -0300

    iniciando a prova da getnet

---
+++ cypress/e2e/apiAddUser.cy copy.js
@@ -0,0 +1,13 @@
+describe("API tests that add new users", () => {
+
+    //add a new user
+    //adicionar um novo usuario, enviando o nome no body em branco
+    //adicionar um novo usuario, enviando o job no body em branco
+    //test do jasonSchema
+    //vaiidar o status code
+    //validar a data de cadastrado, deve ser iqual a data de hoje
+    //validar se retornou o id 
+    //fazer o assert no nome do usuario cadastrado
+    //fazer o asser no cadastro do JOb
+
+})
---
+++ cypress/e2e/apiList.cy.js
@@ -0,0 +1,7 @@
+describe("Testes da api de listagem", () => {
+
+    //testar listagem retornando 200
+    //testar listagem vazia
+    //validar o jasonSchema
+  
+})
